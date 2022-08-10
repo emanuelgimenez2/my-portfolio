20c75c404b002aebfbec0d069fe09fdd931e5437
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2022-08-09 22:07:55 -0300

    quien soy

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ export const headerData = {
   name: "Emanuel Gimenez",
   title: "Web Developer",
   desciption:
-    " as in charge of  groups, aligning myself with the objectives of the  business. Eager to learn and constant training in FreeCodeCamp, Platzi and soyHenry generating projects ",
+    "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. -Thomas A. Edison",
   image: "https://avatars.githubusercontent.com/u/75394638?v=4",
   resumePdf:
     "https://drive.google.com/file/d/1j2Vs_FrHvDRJB87hCW_xVWjxVyAkVKhl/view?usp=sharing",
