2e2ff0846ee09ea299c4ca6e57ea1ece5bf035d3
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2022-08-09 22:04:24 -0300

    quien soy

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: " work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name is Emanuel. I'm a web developer based in Entre Ríos, AR.",
+    description2: " I started my path in development at the university of systems and continued specializing in Platzi, and this last year I graduated from the Bootcamp soyHenry as a Full Stack Developer. I work on independent projects and take advantage of the time to build my products. I spend my leisure hours reading. Right now I'm finishing a wine E-commerce, which was the final work in the Bootcamp. I love learning and exploring new fields.",
     image: 2
 }
